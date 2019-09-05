df3bbfcd6207faddf5fd4b522f6803dc299c74cd
System Administrator <admin@example.com>
2019-09-05 12:43:07 +0530

    Changes by 'System Administrator' on 'kavya test' -- Build #4

---
+++ sn/Global/Script Include/active_true/global.myScriptInclude.js
@@ -0,0 +1,16 @@
+/* 
+ * Application : Global
+ * ClassName   : sys_script_include
+ * Created On  : 2019-09-05 07:06:37
+ * Created By  : admin
+ * Updated On  : 2019-09-05 07:06:37
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=91c755882ff3b3008ae5ddda2799b6ff
+ */
+var myScriptInclude = Class.create();
+myScriptInclude.prototype = {
+    initialize: function() {
+    },
+
+    type: 'myScriptInclude'
+};
